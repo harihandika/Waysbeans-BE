d87b354237be28e91e610cf400240747c5276101
Hari Handika Setiawan <harihandika456@gmail.com>
2022-10-06 09:35:34 +0700

    BE-Waysbeans

--- dto/product/product_request.go
+++ dto/product/product_request.go
@@ -4,7 +4,6 @@ type ProductRequest struct {
 	Title string `json:"title" form:"title" gorm:"type:varchar(255)" validate:"required"`
 	Price int    `json:"price" gorm:"type: int" form:"price" validate:"required"`
 	Image string `json:"image" form:"image" gorm:"type:varchar(255)"`
-	Im    string `json:"im" gorm:"type: text"`
 	Desc  string `json:"desc" form:"desc" gorm:"text"`
 	Stock int    `json:"stock" form:"stock" gorm:"type:int"`
 }
@@ -13,7 +12,6 @@ type UpdateProduct struct {
 	Title string `json:"title" form:"title"`
 	Price int    `json:"price" gorm:"type: int" form:"price"`
 	Image string `json:"image" form:"image"`
-	Im    string `json:"im" gorm:"type: text"`
 	Desc  string `json:"desc" form:"desc" gorm:"text"`
 	Stock int    `json:"stock" form:"stock" gorm:"type:int"`
 }
